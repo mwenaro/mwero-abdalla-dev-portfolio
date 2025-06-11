29a1b6740642361839530538f0ce5d8d5f874fa5
Mwero Abdalla <mwerothewebmaker@gmail.com>
2025-06-11 16:06:55 +0300

    fix : fix bug in ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
-import { ThemeProvider } from "@/components/ThemeProvider";
-
-
+import { ThemeProvider } from "@/components/providers/ThemeProdiver";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en"  suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
---
+++ src/components/providers/ThemeProdiver.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { PropsWithChildren } from "react";
+
+export function ThemeProvider({ children }: PropsWithChildren) {
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </NextThemesProvider>
+  );
+}
